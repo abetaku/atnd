7dc1e814b201fae189b354d884e667aebee77fac
abetaku <takuya@abetaku.net>
2013-03-07 16:50:55 +0900

    first

---
+++ atnd_config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+API_URL = 'http://api.atnd.org/eventatnd/event/'
+
+API_PRAM = { 'event_id':'',
+             'keyword':'',
+             'keyword_or':'',
+             'category':'',
+             'category_detail':'',
+             'ym':'',
+             'ymd':'' }
+
+             
+             
+             
